1eccef3cf2d3782b910ce83614524d599cadf9b9
Nekitech <nekitech-x@mail.ru>
2023-01-27 22:47:59 +0300

    add react, typescript, generate-react-cli

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" data-testid="App">
+      hello gays
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Accordion/Accordion.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './Accordion.module.scss';
+
+interface AccordionProps {}
+
+const Accordion: FC<AccordionProps> = () => (
+  <div className={styles.Accordion} data-testid="Accordion">
+    Accordion Component
+  </div>
+);
+
+export default Accordion;
