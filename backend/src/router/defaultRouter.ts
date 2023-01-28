8d71dfe6570d9f0c15de64425671862f0e4756b8
volshebnik09 <shulgin.grigoriy@yandex.ru>
2023-01-28 17:31:54 +0300

    add projectPath.ts

--- backend/src/router/defaultRouter.ts
+++ backend/src/router/defaultRouter.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import {callParser} from "~/src/parsers/parserInterface";
 import {TParserTypes} from "~/src/types/serializables/parser";
+import {__projectPath} from "~/src/utils/projectPath";
 
 const router = Router();
 router.get('/',(reg,res)=>{
     const result = callParser({
         parser: {
-            fileUrl: __dirname + '/../parsers/nodejs/RSCI_grants/index.ts',
+            fileUrl: __projectPath + '\\parsers\\nodejs\\RSCI\\grants.ts',
             parserType: TParserTypes['ts-node'],
             url: 'https://rsci.ru/grants/',
             name: 'RSCI_grants'
