cf87f6a8ed38066273bf8764f32c17a53b51eadc
Nekitech <nekitech-x@mail.ru>
2023-02-01 04:12:35 +0300

    fix parser, add exception / keywords

--- backend/src/parsers/nodejs/fasie/parser.js
+++ backend/src/parsers/nodejs/fasie/parser.js
@@ -1,50 +1,134 @@
-import {getHTML} from "../utils/getHTML.js";
+import { getHTML } from '../utils/getHTML.js';
 
-const url = 'https://fasie.ru/press/'
+const url = 'https://fasie.ru/press/';
 const baseUrl = 'https://fasie.ru';
 
 const querySelectors = {
-    title: 'li.tile_block_x1 h5',
-    link: 'ul.tile_blocks a',
-    date: 'li.tile_block_x1 p span.ico_clock b',
-}
+	title: 'h1[itemprop="name"]',
+	link: 'ul.tile_blocks a',
+	date: 'span.ico_clock b',
+	text: 'div[itemprop="text"]',
+};
+
+const exceptionWords = [
+	'результаты',
+	'результат',
+	'результатам',
+	'результату',
+	'результатов',
+	'итоги',
+	'подведены итоги',
+	'итог',
+	'вебинар',
+	'вебинары',
+	'победитель',
+	'победители',
+	'победителя',
+	'победителей',
+	'победителю',
+	'победителям',
+	'победителем',
+	'победителе',
+	'победителями',
+	'победителях',
+	'посетил ',
+	'посетила ',
+	'посетили ',
+	'посетит ',
+	'встреча ',
+];
+
+const keyWords = [
+	'гранты',
+	'грант ',
+	'гранту',
+	'грантам',
+	'грантом',
+	'гранте',
+	'приём заявок',
+	'прием заявок',
+	'запуск конкурса',
+	'конкурс ',
+	'конкурсы ',
+	'конкурса',
+	'конкурсу',
+	'конкурсом',
+	'конкурсе',
+	'конкурсам',
+	'конкурсами',
+	'конкурсах',
+	'конкурсов',
+	'на реализацию',
+];
 
 const getNamePosts = (jsdom, querySelector) => {
-    return Array.from(jsdom.window.document.querySelectorAll(querySelector)).map((title) => title.textContent);
-}
+	return (
+		jsdom.window.document.querySelector(querySelector)?.textContent ?? ''
+	);
+};
 
 const getLinksPosts = (jsdom, querySelector) => {
-    return Array.from(jsdom.window.document.querySelectorAll(querySelector)).map((link) => baseUrl + link.getAttribute('href'));
-}
+	return Array.from(
+		jsdom.window.document.querySelectorAll(querySelector)
+	).map((link) => baseUrl + link.getAttribute('href'));
+};
 
 const getDatesPosts = (jsdom, querySelector) => {
-    return Array.from(jsdom.window.document.querySelectorAll(querySelector)).map((date) => date.textContent);
-}
+	return (
+		jsdom.window.document.querySelector(querySelector)?.textContent ?? ''
+	);
+};
 
-const getTextPosts = (links) => {
-    return links.map(async (link) => {
-        const jsdom = await getHTML(link);
-        const querySelector = 'div[itemprop="text"]';
-        return Array.from(jsdom.window.document.querySelectorAll(querySelector)).map((text) => text.textContent);
-    })
+const getTextPosts = (jsdom, querySelector) => {
+	return (
+		jsdom.window.document.querySelector(querySelector)?.textContent ?? ''
+	);
+};
 
-}
+const getInfoPosts = (links) => {
+	return links.map(async (link) => {
+		const jsdom = await getHTML(link);
+		const { title, date, text } = querySelectors;
 
-const getPostLazyLoading = async (totalPosts, url, querySelector) => {
-    for(let i = 0; i < totalPosts; i++) {
-        const jsdom = await getHTML(`${url}?ajax=Y&ajax=Y&PAGEN_1=${i}`);
-        const title = getNamePosts(jsdom, querySelector.title);
-        const date = getDatesPosts(jsdom, querySelector.date);
-        const links = getLinksPosts(jsdom, querySelector.link);
-        const text = await Promise.all(getTextPosts(links));
-        console.log(title, date, links, text);
-    }
-}
+		return {
+			title: getNamePosts(jsdom, title),
+			date: getDatesPosts(jsdom, date),
+			text: getTextPosts(jsdom, text).replaceAll('\n', ''),
+			link,
+		};
+	});
+};
 
-getPostLazyLoading(2, url, querySelectors);
+const getPostLazyLoading = async (totalPosts, url, querySelectors) => {
+	const posts = [];
 
-//
-// console.log(getNamePosts(jsdom, querySelectors.title));
-// console.log(getLinksPosts(jsdom, querySelectors.title));
-// console.log(getDatesPosts(jsdom, querySelectors.date));
-// console.logwait Promise.all(getTextPosts(getLinksPosts(jsdom, querySelectors.title)))(a);
+	for (let i = 0; i < totalPosts; i++) {
+		const jsdom = await getHTML(`${url}?ajax=Y&PAGEN_1=${i}`);
+		const links = getLinksPosts(jsdom, querySelectors.link);
+		posts.push(...(await Promise.all(getInfoPosts(links))).slice(0, -1));
+	}
+	return posts;
+};
+
+const filterPosts = (posts) => {
+	return posts
+		.filter((post) => {
+			const { title } = post;
+
+			return keyWords.some(
+				(word) => title.toLowerCase().includes(word)
+				// text.toLowerCase().includes(word)
+			);
+		})
+		.filter((post) => {
+			const { title } = post;
+
+			return exceptionWords.every((word) => {
+				return !title.toLowerCase().includes(word);
+			});
+		});
+};
+
+const gottenPosts = await getPostLazyLoading(10, url, querySelectors);
+
+console.log(filterPosts(gottenPosts), filterPosts(gottenPosts).length);
