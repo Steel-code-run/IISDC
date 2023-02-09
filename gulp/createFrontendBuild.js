f6a459247f895cf4e9f58ad3df61d973f0445a7e
sin <shulgin.grigoriy@yandex.ru>
2023-02-09 17:19:45 +0300

    gulp build

---
+++ gulp/createFrontendBuild.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const {execSync} = require('child_process');
+function clear(cb){
+    try {
+        execSync("rd -r built/frontend")
+    }
+    catch (e) {}
+    cb();
+}
+
+function buildFrontend(cb){
+    execSync("cd frontend && npm run build")
+    cb()
+}
+
+function copyEnv(cb){
+    gulp.src('frontend/.env.*')
+        .pipe(gulp.dest('built/frontend'));
+    cb()
+}
+
+function copyBuild(cb){
+    gulp.src('frontend/build/**/*')
+        .pipe(gulp.dest('built/frontend/build'));
+    cb()
+}
+
+gulp.task("createFrontendBuild", gulp.parallel(
+    clear,
+    buildFrontend,
+    copyEnv,
+    copyBuild
+))
