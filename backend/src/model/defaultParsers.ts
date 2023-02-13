5e3b8fc0903d57b0384d244d2f8ddeaba34d8324
sin <shulgin.grigoriy@yandex.ru>
2023-02-13 22:43:53 +0300

    bug fix regexp.test() Я ненавижу метод test, не понимаю как он работает

--- backend/src/model/defaultParsers.ts
+++ backend/src/model/defaultParsers.ts
@@ -1,36 +1,42 @@
 import {TParser, TParserType} from "@iisdc/types";
 
 const parsersParams = [
+    // {
+    //     name:"cptgrantov",
+    //     url:"https://cptgrantov.ru/",
+    //     fileUrl:"cptgrantov",
+    //     parserType: TParserType["nodejs"]
+    // },
+    // {
+    //     name:"fadm.gov",
+    //     url:"https://fadm.gov/",
+    //     fileUrl:"fadm.gov",
+    //     parserType: TParserType["nodejs"]
+    // },
+    // {
+    //     name:"fasie",
+    //     url:"https://fasie.ru/",
+    //     fileUrl:"fasie",
+    //     parserType: TParserType["nodejs"]
+    // },
+    // {
+    //     name:"integraciya",
+    //     url:"https://integraciya.ru/",
+    //     fileUrl:"integraciya",
+    //     parserType: TParserType["nodejs"]
+    // },
     {
-        name:"cptgrantov",
-        url:"https://cptgrantov.ru/",
-        fileUrl:"cptgrantov",
+        name:"RSCI",
+        url:"https://RSCI.ru/",
+        fileUrl:"RSCI",
         parserType: TParserType["nodejs"]
     },
-    {
-        name:"fadm.gov",
-        url:"https://fadm.gov/",
-        fileUrl:"fadm.gov",
-        parserType: TParserType["nodejs"]
-    },
-    {
-        name:"fasie",
-        url:"https://fasie.ru/",
-        fileUrl:"fasie",
-        parserType: TParserType["nodejs"]
-    },
-    {
-        name:"integraciya",
-        url:"https://integraciya.ru/",
-        fileUrl:"integraciya",
-        parserType: TParserType["nodejs"]
-    },
-    {
-        name:"minobrnauki",
-        url:"https://minobrnauki.ru/",
-        fileUrl:"minobrnauki",
-        parserType: TParserType["nodejs"]
-    },
+    // {
+    //     name:"minobrnauki",
+    //     url:"https://minobrnauki.ru/",
+    //     fileUrl:"minobrnauki",
+    //     parserType: TParserType["nodejs"]
+    // },
 ]
 export const generateDefaultParsers = ():TParser[] => {
     const defaultParsers: TParser[] = [];
