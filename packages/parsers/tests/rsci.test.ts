000122255e947b8a23cf036cfbe6b2ddb4bf8ea0
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-03-18 20:56:56 +0300

    RSCI tests

---
+++ packages/parsers/tests/rsci.test.ts
@@ -0,0 +1,73 @@
+import {callParser} from "../index";
+import {TParserCallParams, TParserResult} from "@iisdc/types";
+import {parserCallParamsFixture} from "./fixtures/parserCallParams";
+import { beforeEach, describe, expect, jest, test } from '@jest/globals';
+
+const parserFileUrl = "RSCI"
+
+let parserCallParams = parserCallParamsFixture(parserFileUrl)
+let dataPromise:  Promise<TParserResult>;
+let dataFromPage2Promise:  Promise<TParserResult>;
+
+describe(parserFileUrl,()=>{
+    beforeAll(()=>{
+        dataPromise = callParser(parserCallParams)
+        dataFromPage2Promise = callParser({...parserCallParams, page: 2})
+    })
+
+    test(":: have some posts",()=>{
+        return dataPromise.then(data=>{
+            expect(data.length).toBeGreaterThan(0)
+        })
+    })
+
+    test(":: all posts contains postType", ()=>{
+        return dataPromise.then(data=>{
+            let res = true;
+            data.forEach((el)=>{
+                if (el.postType.length < 1)
+                    res = false
+            })
+            expect(res).toBe(true)
+        })
+    })
+
+    test(":: all posts contains namePost", ()=>{
+        return dataPromise.then(data=>{
+            let res = true;
+            data.forEach((el)=>{
+                if (el.postDescription.namePost.length < 1)
+                    res = false
+            })
+            expect(res).toBe(true)
+        })
+
+    })
+
+    test(":: all posts contains link", ()=>{
+        return dataPromise.then(data=>{
+            let res = true;
+            data.forEach((el)=>{
+                if (el.postDescription.link.length < 1)
+                    res = false
+            })
+            expect(res).toBe(true)
+        })
+    })
+
+    test(":: page 2 started with new posts", ()=>{
+        return dataPromise.then(data=>{
+            return dataFromPage2Promise.then(dataFromPage2=>{
+                let res = true;
+                dataFromPage2.forEach((el,index)=>{
+                    if ((dataFromPage2[index].postDescription.namePost ===
+                        data[index].postDescription.namePost) && (dataFromPage2[index].postDescription.link ===
+                        data[index].postDescription.link))
+                        res = false
+                })
+                expect(res).toBe(true)
+            })
+        })
+    })
+
+})
