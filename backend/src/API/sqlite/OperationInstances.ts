0d1675b3c85d7a8f5a8b859e187b68930bd1bb30
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-03-26 20:35:08 +0300

    dd transferFile, change timeout

--- backend/src/API/sqlite/OperationInstances.ts
+++ backend/src/API/sqlite/OperationInstances.ts
@@ -1,5 +1,6 @@
 import {DirectionsConstOperations} from "./DirectionsConstOperations";
 import {
+    competitionsTableName,
     directionsConstTableName,
     directionsTableName,
     grantsTableName,
@@ -17,7 +18,7 @@ import {UsersOperations} from "./users/UsersOperations";
 export const directionsConstOperations = new DirectionsConstOperations(parserDb, directionsConstTableName)
 export const directionsOperations = new DirectionsOperations(parserDb, directionsTableName, directionsConstOperations)
 export const grantsOperations = new GrantsOperations(parserDb,grantsTableName, directionsOperations)
-export const competitionsOperations = new CompetitionOperations(parserDb,grantsTableName, directionsOperations)
+export const competitionsOperations = new CompetitionOperations(parserDb,competitionsTableName, directionsOperations)
 export const internshipOperations = new InternshipOperations(parserDb,internshipsTableName)
 export const vacanciesOperations = new VacanciesOperations(parserDb,vacanciesTableName)
 export const usersOperations = new UsersOperations(parserDb,usersTableName)
