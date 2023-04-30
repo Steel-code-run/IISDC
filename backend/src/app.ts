64a86c51721a1e98f97ad9ba8ca13e2e44ebb717
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-04-30 21:41:09 +0300

    cors update whitelist

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import baseRouter from "./router/baseRouter";
 import grantsRouter from "./router/v1/grantsRouter";
-import {connect} from "./prisma/connect";
+import prisma, {connect} from "./prisma/connect";
 import usersRouter from "./router/v1/usersRouter";
 import rolesRouter from "./router/v1/rolesRouter";
 
@@ -12,19 +12,25 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3003;
 
-const whitelist = [
-	"0.0.0.0",
-	"localhost"
-]
-
 // Пока связи с бд нет, приложение не запускается
-connect().then(_ => {
+connect().then(async _ => {
 	console.log("connected to db")
 
 	const corsOptions = {
 		credentials: true, //access-control-allow-credentials:true,
 		exposedHeaders: 'Authorization',
-		origin: function (origin:any, callback:any) {
+		origin:async function (origin:any, callback:any) {
+			let whitelist:any = [];
+			try {
+				whitelist = await prisma.whitelist.findMany().then((res) => {
+					return res.map((item) => {
+						return item.origin
+					})
+				});
+			} catch (e) {
+				console.log(e)
+				callback(new Error('Not allowed by CORS'))
+			}
 			if (whitelist.indexOf(origin) !== -1) {
 				callback(null, true)
 			} else {
