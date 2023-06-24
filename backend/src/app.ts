890305f12328c05959f6c95d33d195968e65b9c8
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-06-24 18:37:52 +0300

    fix compiling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -38,7 +38,7 @@ connect().then(async _ => {
 
 	app.set('views', path.join(__dirname, 'views'))
 	app.set('view engine', 'ejs');
-	app.use(express.static(__dirname+'/public'));
+	app.use('/public', express.static(__dirname+'/public'));
 
 	// плагины
 	app.use(cors(corsOptions));
