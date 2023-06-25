bd4dee93f8633ef2e49c5c156eabf7f6be8a8157
Nekitech <nekitech-x@mail.ru>
2023-06-25 12:28:24 +0300

    hot fix: token

--- admin/src/config/apiConfig.js
+++ admin/src/config/apiConfig.js
@@ -1,4 +1,4 @@
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNjg2NTE2MjUzLCJleHAiOjE2ODY2MDI2NTN9.Znx-dkz1juwRfEVlNiQAuyskrKQ98oaJ4xZSIkRYThk'
+export const token = window.sessionStorage.getItem('token')
 
 export const defaultHeaders = {
     Authorization: 'Bearer ' + token
