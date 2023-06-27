218fe195eecee5f5c0fb487e2268aaab827796fe
Шульгин Григорий <shulgin.grigoriy@yandex.ru>
2023-06-27 21:28:58 +0300

    fix

--- backend/src/telegram/on/onCallbackQuery/onGrants_get.ts
+++ backend/src/telegram/on/onCallbackQuery/onGrants_get.ts
@@ -3,6 +3,7 @@ import {createButton} from "../../functions/Button";
 import {param} from "express-validator";
 import {grants_keyboard} from "../../keyboards";
 import prisma from "../../../prisma/connect";
+import {getUserTelegramSettingsOrCreate} from "../../functions/getUserTelegramSettingsOrCreate";
 
 export const onGrants_get = async (props:OnSomethingProps) => {
     const {bot, chatId, callbackQuery, user} = props
@@ -13,6 +14,9 @@ export const onGrants_get = async (props:OnSomethingProps) => {
     }
 
     let directions;
+    if (!user){
+        return
+    }
     if (user?.user_telegram_settingsId) {
         const user_telegram_settings = await prisma.users_telegram_settings.findFirst({
             where: {
@@ -22,11 +26,16 @@ export const onGrants_get = async (props:OnSomethingProps) => {
         if (user_telegram_settings?.directions) {
             directions = JSON.parse(user_telegram_settings.directions)
         }
+    } else {
+        const user_telegram_settings = await getUserTelegramSettingsOrCreate(user)
     }
 
     let where:any = {
         OR: []
     }
+
+    if (!directions?.length)
+        directions = []
 
     for (let i = 0; i < directions.length; i++) {
         where.OR.push({
