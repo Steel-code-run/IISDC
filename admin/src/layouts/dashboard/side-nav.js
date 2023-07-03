9473e1ccd23d8c91839ebc008cd1f224e35924ef
Nekitech <nekitech-x@mail.ru>
2023-07-03 21:13:50 +0300

    hot fix: add link for instructions admins

--- admin/src/layouts/dashboard/side-nav.js
+++ admin/src/layouts/dashboard/side-nav.js
@@ -2,164 +2,175 @@ import NextLink from 'next/link';
 import {usePathname} from 'next/navigation';
 import PropTypes from 'prop-types';
 import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
-import {Box, Divider, Drawer, Stack, SvgIcon, Typography, useMediaQuery} from '@mui/material';
+import {Box, Button, Divider, Drawer, Stack, SvgIcon, Typography, useMediaQuery} from '@mui/material';
 import {Scrollbar} from 'src/components/scrollbar';
 import {items} from './config';
 import {SideNavItem} from './side-nav-item';
 
 export const SideNav = (props) => {
-  const { open, onClose } = props;
-  const pathname = usePathname();
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
+    const {open, onClose} = props;
+    const pathname = usePathname();
+    const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
 
-  const content = (
-    <Scrollbar
-      sx={{
+    const content = (
+        <Scrollbar
+            sx={{
 
-        height: '100%',
-        '& .simplebar-content': {
-          height: '100%'
-        },
-        '& .simplebar-scrollbar:before': {
-          background: 'neutral.400'
-        }
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%'
-        }}
-      >
-        <Box sx={{ p: 3 }}>
-          <Box
-            component={NextLink}
-            href="/"
-            sx={{
-              display: 'inline-flex',
-              height: 32,
-              width: 32
+                height: '100%',
+                '& .simplebar-content': {
+                    height: '100%'
+                },
+                '& .simplebar-scrollbar:before': {
+                    background: 'neutral.400'
+                }
             }}
-          >
-            {/*<Logo />*/}
-          </Box>
-          <Box
-            sx={{
-              alignItems: 'center',
-              backgroundColor: 'rgba(255, 255, 255, 0.04)',
-              borderRadius: 1,
-              cursor: 'pointer',
-              display: 'flex',
-              justifyContent: 'space-between',
-              mt: 2,
-              p: '12px'
-            }}
-          >
-            <div>
-              <Typography
-                color="inherit"
-                variant="subtitle1"
-              >
-                  Прототип информационно - поисковой системы сбора данных о грантах, конкурсах и стажировках
-              </Typography>
-              <Typography
-                color="neutral.400"
-                variant="body2"
-              >
-                Админ-панель
-              </Typography>
-            </div>
-            <SvgIcon
-              fontSize="small"
-              sx={{ color: 'neutral.500' }}
-            >
-              <ChevronUpDownIcon />
-            </SvgIcon>
-          </Box>
-        </Box>
-        <Divider sx={{ borderColor: 'neutral.700' }} />
-        <Box
-          component="nav"
-          sx={{
-            flexGrow: 1,
-            px: 2,
-            py: 3
-          }}
         >
-          <Stack
-            component="ul"
-            spacing={0.5}
-            sx={{
-              listStyle: 'none',
-              p: 0,
-              m: 0
-            }}
-          >
-            {items.map((item) => {
-              const active = item.path ? (pathname === item.path) : false;
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%'
+                }}
+            >
+                <Box sx={{p: 3}}>
+                    <Box
+                        component={NextLink}
+                        href="/"
+                        sx={{
+                            display: 'inline-flex',
+                            height: 32,
+                            width: 32
+                        }}
+                    >
+                        {/*<Logo />*/}
+                    </Box>
+                    <Box
+                        sx={{
+                            alignItems: 'center',
+                            backgroundColor: 'rgba(255, 255, 255, 0.04)',
+                            borderRadius: 1,
+                            cursor: 'pointer',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            mt: 2,
+                            p: '12px'
+                        }}
+                    >
+                        <div>
+                            <Typography
+                                color="inherit"
+                                variant="subtitle1"
+                            >
+                                Прототип информационно - поисковой системы сбора данных о грантах, конкурсах и
+                                стажировках
+                            </Typography>
+                            <Typography
+                                color="neutral.400"
+                                variant="body2"
+                            >
+                                Админ-панель
+                            </Typography>
+                        </div>
+                        <SvgIcon
+                            fontSize="small"
+                            sx={{color: 'neutral.500'}}
+                        >
+                            <ChevronUpDownIcon/>
+                        </SvgIcon>
+                    </Box>
+                </Box>
+                <Divider sx={{borderColor: 'neutral.700'}}/>
+                <Box
+                    component="nav"
+                    sx={{
+                        flexGrow: 1,
+                        px: 2,
+                        py: 3
+                    }}
+                >
+                    <Stack
+                        component="ul"
+                        spacing={0.5}
+                        sx={{
+                            listStyle: 'none',
+                            p: 0,
+                            m: 0
+                        }}
+                    >
+                        {items.map((item) => {
+                            const active = item.path ? (pathname === item.path) : false;
 
-              return (
-                <SideNavItem
-                  active={active}
-                  disabled={item.disabled}
-                  external={item.external}
-                  icon={item.icon}
-                  key={item.title}
-                  path={item.path}
-                  title={item.title}
-                  childrenNav={item.childrenNav}
-                />
-              );
-            })}
-          </Stack>
-        </Box>
-        <Divider sx={{ borderColor: 'neutral.700' }} />
+                            return (
+                                <SideNavItem
+                                    active={active}
+                                    disabled={item.disabled}
+                                    external={item.external}
+                                    icon={item.icon}
+                                    key={item.title}
+                                    path={item.path}
+                                    title={item.title}
+                                    childrenNav={item.childrenNav}
+                                />
+                            );
+                        })}
+                    </Stack>
+                </Box>
+                <Button
+                    variant="outlined"
+                    target={'_blank'}
+                    href={'https://docs.google.com/document/d/1UqoLLA5_t8yp4E_c_URqi-RQU41ObAmMFuB4uQc5g_U/edit?usp=sharing'}
+                    sx={{
+                        color: '#fff',
+                        margin: '0 10px 20px 10px'
+                    }}>
+                    Инструкция для админов
+                </Button>
+                <Divider sx={{borderColor: 'neutral.700'}}/>
 
-      </Box>
-    </Scrollbar>
-  );
-
-  if (lgUp) {
-    return (
-      <Drawer
-        anchor="left"
-        open
-        PaperProps={{
-          sx: {
-            backgroundColor: 'neutral.800',
-            color: 'common.white',
-            width: 280
-          }
-        }}
-        variant="permanent"
-      >
-        {content}
-      </Drawer>
+            </Box>
+        </Scrollbar>
     );
-  }
 
-  return (
-    <Drawer
-      anchor="left"
-      onClose={onClose}
-      open={open}
-      PaperProps={{
-        sx: {
-          backgroundColor: 'neutral.800',
-          color: 'common.white',
-          width: 280
-        }
-      }}
-      sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
-      variant="temporary"
-    >
-      {content}
-    </Drawer>
-  );
+    if (lgUp) {
+        return (
+            <Drawer
+                anchor="left"
+                open
+                PaperProps={{
+                    sx: {
+                        backgroundColor: 'neutral.800',
+                        color: 'common.white',
+                        width: 280
+                    }
+                }}
+                variant="permanent"
+            >
+                {content}
+            </Drawer>
+        );
+    }
+
+    return (
+        <Drawer
+            anchor="left"
+            onClose={onClose}
+            open={open}
+            PaperProps={{
+                sx: {
+                    backgroundColor: 'neutral.800',
+                    color: 'common.white',
+                    width: 280
+                }
+            }}
+            sx={{zIndex: (theme) => theme.zIndex.appBar + 100}}
+            variant="temporary"
+        >
+            {content}
+        </Drawer>
+    );
 };
 
 SideNav.propTypes = {
-  onClose: PropTypes.func,
-  open: PropTypes.bool
+    onClose: PropTypes.func,
+    open: PropTypes.bool
 };
